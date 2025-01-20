229d149f76706d61eb64bd2ac2014cbfba26ec7d
wkzhiro <wkzhiro0116@yahoo.co.jp>
2025-01-20 22:08:26 +0900

    login実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* グローバルにSessionProviderを適用 */}
-        <Providers>{children}
+        <Providers>
           <DataProvider>
             {children}
           </DataProvider>
